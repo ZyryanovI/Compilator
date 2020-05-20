942ba0475f8db65786b7a9e6ad3f83e336f8c2d7
DariaFil <36414448+DariaFil@users.noreply.github.com>
2020-05-20 23:31:38 +0300

    Update CodeFragment.h

--- IRTree/CodeFragment.h
+++ IRTree/CodeFragment.h
@@ -1,5 +1,10 @@
+#pragma once
+#include "../SymbolTable/Symbol.h"
+#include "IRExpression.h"
 #include "IRStatement.h"
-#include "../ActivationRecords/IFrame.h"
+#include "Expression.h"
+#include <string>
+#include <map>
 
 class CodeFragment {
 public:
@@ -13,4 +18,64 @@ public:
     IFrame* frame;
     IRStatement* body;
     std::unique_ptr<const IRStatement> rootCanonIRT;
+};
+
+class IAccess {
+public:
+    virtual ~IAccess() = default;
+    virtual IRExpression* GetExp() = 0;
+};
+
+class InFrameAccess : public IAccess {
+public:
+    InFrameAccess(IAccess* address, int offset);
+    IRExpression* GetExp() override;
+
+private:
+    IAccess* address;
+    int offset;
+};
+
+class InRegAccess : public IAccess {
+public:
+    InRegAccess(std::string name);
+    IRExpression* GetExp() override;
+
+private:
+    std::string name;
+};
+
+class IFrame {
+public:
+    virtual ~IFrame() = default;
+    virtual void AddFormal(const std::string& name) = 0;
+    virtual void AddLocal(const std::string& name) = 0;
+    virtual std::string GetName() = 0;
+    virtual IRExpression* ExternalCall(std::string& method_name, IRExpression* args) = 0;
+    virtual IAccess* GetAccess(const std::string& access_name) = 0;
+    virtual int WordSize() = 0;
+};
+
+class X86MiniJavaFrame : public IFrame {
+public:
+    X86MiniJavaFrame(Symbol* class_name, Symbol* method_name);
+    std::string GetName() override {
+        return name;
+    }
+    void AddFormal(const std::string& formal_name) override;
+    void AddLocal(const std::string& local_name) override;
+    IAccess* GetAccess(const std::string& access_name) override;
+    IRExpression* ExternalCall(std::string& method_name, IRExpression* args) override;
+    int WordSize() override {
+        return word_size;
+    }
+
+    const static std::string frame_pointer;
+    const static std::string this_pointer;
+    const static std::string return_pointer;
+private:
+    std::string name;
+    int frame_size;
+    const int word_size = 4;
+    std::map<std::string, IAccess*> addresses;
 };
