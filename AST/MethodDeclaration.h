6237d7e2a612e92b74edad5a3c8c0fb750c7c0d7
Piscenco Margarita <32290501+ceasminog@users.noreply.github.com>
2020-05-24 18:52:27 +0300

    changed Method Declaration

--- AST/MethodDeclaration.h
+++ AST/MethodDeclaration.h
@@ -1,6 +1,9 @@
 #pragma once
 
-#include "Interfaces.h"
+#include "Interfaces.hpp"
+#include "../Visitor.h"
+
+#include <iostream>
 #include <vector>
 #include <memory>
 #include <utility>
@@ -15,10 +18,27 @@ public:
     std::unique_ptr<std::vector<std::unique_ptr<IStatement>>> statements;
     std::unique_ptr<IExpression> return_expression;
 
-    MethodDeclaration(int first_line, int first_column, IType* return_type, IIdentifier* name,
-            std::vector<std::pair<std::unique_ptr<IType>, std::unique_ptr<IIdentifier>>>* args,
-            std::vector<std::unique_ptr<IVarDeclaration>>* vars,
-            std::vector<std::unique_ptr<IStatement>>* statements, IExpression* return_expression);
+    // конструктор
+    MethodDeclaration(int first_line, int first_column, IType *return_type, IIdentifier *name,
+        std::vector<std::pair<std::unique_ptr<IType>, std::unique_ptr<IIdentifier>>> *args, std::vector<std::unique_ptr<IVarDeclaration>> *vars,
+        std::vector<std::unique_ptr<IStatement>> *statements, IExpression *return_expression) {
 
-    void Accept(Visitor* visitor) const;
+    if (return_type == nullptr || name == nullptr || args == nullptr || vars == nullptr || statements == nullptr || return_expression == nullptr) {
+        throw "There is nullptr in MethodDeclaration initialization";
+    }
+
+    this->first_line = first_line;
+    this->first_column = first_column;
+
+    this->return_type = std::unique_ptr<IType>(return_type);
+    this->name = std::unique_ptr<IIdentifier>(name);
+    this->args = std::unique_ptr<std::vector<std::pair<std::unique_ptr<IType>, std::unique_ptr<IIdentifier>>>>(args);
+
+    this->vars = std::unique_ptr<std::vector<std::unique_ptr<IVarDeclaration>>>(vars);
+    this->statements = std::unique_ptr<std::vector<std::unique_ptr<IStatement>>>(statements);
+    this->return_expression = std::unique_ptr<IExpression>(return_expression);
+}
+
+void Accept(Visitor* visitor) const {
+    visitor->visit(this);
 };
