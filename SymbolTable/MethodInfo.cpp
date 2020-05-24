29137615667b15660fbb7bc36d384744554dbbe5
DariaFil <36414448+DariaFil@users.noreply.github.com>
2020-05-24 18:59:00 +0300

    Deleting old version

--- SymbolTable/MethodInfo.cpp
+++
@@ -1,5 +0,0 @@
-#include "MethodInfo.h"
-
-MethodInfo::MethodInfo(Type *type, Symbol *name, int first_line, int first_column)
-        : type(type), name(name), first_line(first_line), first_column(first_column) {}
-
