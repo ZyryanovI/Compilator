4bcbd298f756b9a58d990ebdc1fb0aa93ffe6772
Piscenco Margarita <32290501+ceasminog@users.noreply.github.com>
2020-05-24 19:24:57 +0300

    small changes in Identifier

--- AST/Identifier.h
+++ AST/Identifier.h
@@ -1,14 +1,27 @@
 #pragma once
+
 #include "Interfaces.h"
-#include <string>
+#include "../Visitor.h"
 #include "../SymbolTable/Symbol.h"
+
+#include <string>
+
 
 class Identifier : public IIdentifier {
 public:
     Symbol* name;
 
-    Identifier(int first_line, int first_column, Symbol* name);
+Identifier(int first_line, int first_column, Symbol* name) {
+    if (name == nullptr) {
+        std::cout << "Empty name in Identifier!" << std::endl;
+    }
+    this->first_line = first_line;
+    this->first_column = first_column;
+    this->name = name;
+}
 
-    void Accept(Visitor* visitor) const;
+void Accept(Visitor* visitor) const {
+    visitor->visit(this);
+}
 
 };
