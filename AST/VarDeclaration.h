748b67dcdc4bd8b7a013be309c5ecfc811af476a
Piscenco Margarita <32290501+ceasminog@users.noreply.github.com>
2020-05-24 20:04:41 +0300

    added constructor

--- AST/VarDeclaration.h
+++ AST/VarDeclaration.h
@@ -1,14 +1,24 @@
 #pragma once
 
-#include "Interfaces.h"
-#include <memory>
+#include "Interfaces.hpp"
 
 class VarDeclaration : public IVarDeclaration {
 public:
     std::unique_ptr<IType> type;
     std::unique_ptr<IIdentifier> name;
 
-    VarDeclaration(int first_line, int first_column, IType* type, IIdentifier* name);
+    VarDeclaration(int first_line, int first_column, IType *type, IIdentifier *name) {
+    if (type == nullptr || name == nullptr) {
+        std::cout<< "There is nullptr in VarDeclaration initialization"<<std::endl;
+    }
 
-    void Accept(Visitor* visitor) const;
+    this->first_line = first_line;
+    this->first_column = first_column;
+    this->type = std::unique_ptr<IType>(type);
+    this->name = std::unique_ptr<IIdentifier>(name);
+    }
+
+    void Accept(Visitor* visitor) const {
+    visitor->visit(this);
+    }
 };
