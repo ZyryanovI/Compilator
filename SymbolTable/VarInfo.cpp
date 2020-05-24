de7baa2a694b981d18a7a5795426a37b41fa16ed
DariaFil <36414448+DariaFil@users.noreply.github.com>
2020-05-24 19:00:08 +0300

    Deleting old version

--- SymbolTable/VarInfo.cpp
+++
@@ -1,5 +0,0 @@
-#include "VarInfo.h"
-
-VariableInfo::VariableInfo(Type *type, Symbol *symbol, int first_line, int first_column)
-        : type(type), symbol(symbol), first_line(first_line), first_column(first_column) {}
-
