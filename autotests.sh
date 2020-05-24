7da14af54093a4d4ffe928deccf8e9c8f48e5753
zyrjanov ilja <zyryanov.ilya.e@yandex.ru>
2020-05-24 14:07:30 +0300

    autotests added

---
+++ autotests.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+GOOD_SAMPLES="$(ls Samples/ -1)"
+positive_result='successfully ended'
+
+for f in $GOOD_SAMPLES
+do
+    OUTPUT="$(./out Samples/$f)"
+    if [ "$OUTPUT" != "$positive_result" ]
+    then
+        echo "error in $f file (this should be correct file)"
+    fi
+done
+
+
+echo ""
+
+
+BAD_SAMPLES="$(ls BadSamples/ -1)"
+
+for f in $BAD_FILES
+do
+    OUTPUT="$(./out BadSamples/$f)"
+    if [ "$OUTPUT" == "$positive_result" ]
+    then
+        echo "no error in $f file (but they myst be)"
+    fi
+done
